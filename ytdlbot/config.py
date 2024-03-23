003c0a7decc75c49688dea99c2c170b1ed86249f
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-03-23 11:30:34 +0530

    Update config.py

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -49,7 +49,7 @@ MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASS = os.getenv("MYSQL_PASS", "root")
 
 AUDIO_FORMAT = os.getenv("AUDIO_FORMAT")
-ARCHIVE_ID = os.getenv("ARCHIVE_ID")
+ARCHIVE_ID = os.getenv("@Dilshan123")
 
 IPv6 = os.getenv("IPv6", False)
 ENABLE_FFMPEG = os.getenv("ENABLE_FFMPEG", False)
